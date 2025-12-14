3662c56a7e493ac955f5a793f5963624b1079df6
kimkimsun <116052108+kimkimsun@users.noreply.github.com>
2025-12-14 19:22:35 +0900

    ...

--- Assets/JsonLoader.cs
+++ Assets/JsonLoader.cs
@@ -1,16 +1,55 @@
+using System.Collections.Generic;
+using NUnit.Framework.Interfaces;
 using UnityEngine;
 
-public class JsonLoader : MonoBehaviour
+[System.Serializable]
+public struct TestData
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public int a;
+    public int b;
+}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+[System.Serializable]
+public struct Test2Data
+{
+    public string a;
+    public List<int> b;
+    public float c;
+}
+
+public static class JsonLoader
+{
+    //public static T Load<T> (string fileName)
+    //{
+    //    TextAsset[] files = Resources.LoadAll<TextAsset>("");
+
+    //    foreach (TextAsset file in files)
+    //    {
+    //        if(file.name == "")
+    //        {
+
+    //        }
+    //        else if (file.name == "")
+    //        {
+                
+    //        }
+    //    }
+
+
+    //void LoadTest()
+    //{
+    //    TextAsset json = Resources.Load<TextAsset>("test");
+    //    TestData data = JsonUtility.FromJson<TestData>(json.text);
+
+    //    Debug.Log($"test.json ¡æ a: {data.a}, b: {data.b}");
+    //}
+
+    //void LoadTest2()
+    //{
+    //    TextAsset json = Resources.Load<TextAsset>("test2");
+    //    Test2Data data = JsonUtility.FromJson<Test2Data>(json.text);
+
+    //    Debug.Log($"test2.json ¡æ a: {data.a}, c: {data.c}");
+    //    Debug.Log($"b count: {data.b.Count}");
+    //}
 }
